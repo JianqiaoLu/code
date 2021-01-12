26582869375ac71a0cdf088e56ac30101e163c25
lujianqiao <lujianq@gpu2gate1.gpufarm2.cs.hku.hk>
2021-01-12 12:59:46 +0800

    sss

--- fuc.py
+++ fuc.py
@@ -1,20 +1,20 @@
 global eps, m
 def func(alpha):
   global eps, m
-  import pdb 
-  pdb.set_trace()
-  
   return alpha * (0.75 * alpha**(1/2) -  0.25 * alpha **(1.5) + 0.5)
 def func2(alpha):
   global eps, m
-  import pdb 
-  pdb.set_trace()
-  
-  return alpha*( - alpha**1.5**((eps/m)**2) + alpha *(2*(eps/m)**2 - eps/m) + alpha**0.5*(2*eps/m - (eps/m)**2) + 1)
+  return alpha*( - alpha**1.5*((eps/m)**2) + alpha *(2*(eps/m)**2 - eps/m) + alpha**0.5*(2*eps/m - (eps/m)**2) + 1)
 
 def func3(alpha):
   global eps, m
   return (eps/m) **2 *(2*alpha - alpha**1.5 - alpha**0.5) + eps/m *(2 * alpha**0.5 - alpha - 1)  + 1
+
+def func4(alpha):
+  return alpha * (1 - (alpha**0.5 - 1)**2 * eps /m)
+
+def func5(alpha):
+  return alpha * (1 - alpha* eps /m)
 eps = 1e-3
 m = 10
 initial_alpha = 100.00
